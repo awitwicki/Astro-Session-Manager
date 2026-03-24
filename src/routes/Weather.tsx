9d4f08f3bbad319da17ff8202481a70812de794a
Andrew Witwicki <vitvitskiy3@gmail.com>
2026-03-24 21:48:02 +0100

    Fix location icon in dev

--- src/routes/Weather.tsx
+++ src/routes/Weather.tsx
@@ -8,6 +8,8 @@ import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
 // Fix Leaflet default marker icon path broken by Vite bundling
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+delete (L.Icon.Default.prototype as any)._getIconUrl
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon,
   iconRetinaUrl: markerIcon2x,
